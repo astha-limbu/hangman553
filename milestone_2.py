e74797be54bb29ff7f2c2d14c00e0551325cac37
Astha Limbu <astha.limbu99@gmail.com>
2024-05-22 00:20:31 +0100

    refactor - Meaningful Naming

--- milestone_2.py
+++ milestone_2.py
@@ -3,11 +3,11 @@ import random
 word_list = ['Lychee', 'Mango', 'Papaya', 'Orange', 'Guava']
 print(word_list)
 
-word = random.choice(word_list)
-print(word)
+random_word_pick = random.choice(word_list)
+print(random_word_pick)
 
-guess = input("Enter a single letter: ")
-if guess.isalpha() == True and len(guess) == 1:
+user_guess = input("Enter a single letter: ")
+if user_guess.isalpha() == True and len(user_guess) == 1:
     print("Good guess!")
 else:
     print("Oops! That is not a valid input")
